3202adcc45e692700e1df6bdb3bca18208333222
Piyush Dudhakawre <91535278+piyushdude25@users.noreply.github.com>
2023-03-08 12:51:22 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import LanguageChange from "./LanguageChange";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Language Change: </h1>
+
+      <LanguageChange />
+    </div>
+  );
+}
